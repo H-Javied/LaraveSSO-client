3298a158e2d594c18d48752bd6a21fdc659294f2
Hamza Javied <hamzajavied@invotyx.co.uk>
2022-11-01 09:19:48 +0500

    first commit

---
+++ app/Http/Controllers/SSO/SSOController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\SSO;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Http;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class SSOController extends Controller
+{
+    public function userLogin(Request $request)
+    {
+        $request->session()->put("state", $state = Str::random(40));
+        $query = http_build_query([
+            "client_id" => config("auth.client_id"),
+            "redirect_uri" => config("auth.callback"),
+            "response_type" => "code",
+            "scope" => config("auth.scopes"),
+            "state" => $state,
+        ]);
+        return redirect(config("auth.sso_host") . "/oauth/authorize?" . $query);
+    }
+
+    public function getCallback(Request $request)
+    {
+        $state = $request->session()->pull("state");
+        throw_unless(
+            strlen($state) > 0 && $state == $request->state,
+            InvalidArgumentException::class
+        );
+
+        $response = Http::asForm()->post(
+            config("auth.sso_host") . "/oauth/token",
+            [
+                "grant_type" => "authorization_code",
+                "client_id" => config("auth.client_id"),
+                "client_secret" => config("auth.client_secret"),
+                "redirect_uri" => config("auth.callback"),
+                "code" => $request->code
+            ]
+        );
+        $request->session()->put($response->json());
+        //dd($request);
+        return redirect(route("connect"));
+    }
+
+    public function ssoUser(Request $request)
+    {
+        $access_token = $request->session()->get("access_token");
+        $response = Http::withHeaders([
+            "Accept" => "application/json",
+            "Authorization" => "Bearer " . $access_token
+        ])->get(config("auth.sso_host") . "/api/user");
+        // return $response->json();
+        $userArray = $response->json();
+        try {
+            $email = $userArray['email'];
+        } catch (\Throwable $th) {
+            return redirect("login")->withErrors("Failed to get LoggedIn information! Try again.");
+        }
+        $user = User::where("email", $email)->first();
+        if (!$user) {
+            $heyUser = new User;
+            $heyUser->name = $userArray['name'];
+            $heyUser->email = $userArray['email'];
+            $heyUser->email_verified_at = $userArray['email_verified_at'];
+            $heyUser->save();
+        }
+
+        Auth::login($user);
+        return redirect(route("home"));
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\SSO\SSOController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Auth;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get("/login/get", [SSOController::class, 'userLogin'])->name('sso-login');
+Route::get("/callback", [SSOController::class, 'getCallback'])->name("callback");
+Route::get("/authUser", [SSOController::class, 'ssoUser'])->name("connect");
+
+Auth::routes(['register' => false, 'reset' => false]);
+
+Route::get("/home", [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get("/", [App\Http\Controllers\HomeController::class, 'index'])->name('index');
